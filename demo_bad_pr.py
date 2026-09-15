6df6b1838b3edfc34b1342c783a14f9b197e7f8a
raj-k-v <verma.raj5595@kiit.ac.in>
2026-09-15 14:25:08 +0530

    test: trigger PRism review

--- demo_bad_pr.py
+++ demo_bad_pr.py
@@ -64,3 +64,11 @@ def charge(amount):
 def get_user(user_id):
     query = "SELECT * FROM users WHERE id = " + user_id
     return db.execute(query)
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return db.execute(query)
+
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return db.execute(query)
