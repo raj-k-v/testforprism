856109ad2704a1be3d7f4e3e961777eec9fb710f
raj-k-v <verma.raj5595@kiit.ac.in>
2026-09-15 16:11:21 +0530

    k

--- demo_bad_pr.py
+++ demo_bad_pr.py
@@ -133,3 +133,8 @@ def find_user(email: str):
     query = f"SELECT * FROM users WHERE email = '{email}'"
     return connection.execute(query).fetchall() 
 
+def find_user(email: str):
+    connection = sqlite3.connect("demo.db")
+    query = f"SELECT * FROM users WHERE email = '{email}'"
+    return connection.execute(query).fetchall() 
+
