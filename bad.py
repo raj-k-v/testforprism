4174b4eb9c137681cea07a7cfbee802a5f609f52
raj-k-v <verma.raj5595@kiit.ac.in>
2026-09-15 15:31:36 +0530

    k

--- bad.py
+++ bad.py
@@ -4,4 +4,4 @@ import sqlite3
 def find_user(email: str):
     connection = sqlite3.connect("demo.db")
     query = f"SELECT * FROM users WHERE email = '{email}'"
-    return connection.execute(query).fetchall()
+    return connection.execute(query).fetchall() 
